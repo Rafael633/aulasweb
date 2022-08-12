7566116a3edf49cf89d12890c3d389a29343975c
Rafael <rafael.souza2@alunos.sc.senac.br>
2022-08-11 21:48:15 -0300

    Aula 11/08

---
+++ webpack03/src/js/index.js
@@ -0,0 +1,12 @@
+import "../scss/style.scss";
+
+console.log("Hello Lorena");
+
+// variável recebe o nosso aplicativo
+const app = document.getElementById("app");
+// variável recebe a criação de um elemento virtual chamado h1
+const hello = document.createElement('hi');
+// inserimos dentro deste elemento virtual um HTML usando innerHTML
+hello.innerHTML = "Hi bem vindo!";
+//acrescentamos ao elemento 'app' o elemento 'hello'
+app.append(hello);
